7ca237d9efa959b05bd1a9ac84d789e7b3dd97b4
zzzfisherzzz <zzzfisherzzz@gmail.com>
2020-05-31 01:53:48 +0300

    Updates

---
+++ precache-manifest.5130dc1b18ee05936b63c9e8739bf403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65690c7c26ca85ad441fe221965e2ea",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "e46620f5c7701aaae936",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.44eb1dbb.chunk.css"
+  },
+  {
+    "revision": "bc46939fd9f8ca64e520",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a8cacbda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a8cacbda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46620f5c7701aaae936",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.54469aec.chunk.js"
+  },
+  {
+    "revision": "de5dfbc78302bed1e7de",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3cc9cabd.js"
+  }
+]);
